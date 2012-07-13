c8d567e88f3d77e8fb9ae257dde199d62d14b896
WuJun <ghostdom.wj@gmail.com>
2012-07-14 01:28:31 +0800

    添加点点开放平台

---
+++ web.zhengoo.com/app/libraries/Provider/DianDian.php
@@ -0,0 +1,538 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * DianDian OAuth2 Provider
+ *
+ * @package    CodeIgniter/OAuth2
+ * @category   Provider
+ * @author     Ghostdom.wj
+ * @copyright  (c) 2012 HappyNinjas Ltd
+ * @license    http://philsturgeon.co.uk/code/dbad-license
+ */
+define('BLOG_POST_TYPE_ALL', 	'all');		// 博客文章类型: 所有
+define('BLOG_POST_TYPE_TEXT',  	'text'); 	// 博客文章类型: 文字
+define('BLOG_POST_TYPE_PHOTO', 	'photo');	// 博客文章类型: 图片
+define('BLOG_POST_TYPE_AUDIO', 	'audio');	// 博客文章类型: 音乐
+define('BLOG_POST_TYPE_VIDEO', 	'video');	// 博客文章类型: 视频
+define('BLOG_POST_TYPE_LINK',  	'link');	// 博客文章类型: 链接
+
+define('POST_STATE_PUBLISHED', 	'published'); 	// 博文发布状态: 发布
+define('POST_STATE_DRAFT', 		'draft'); 		// 博文发布状态: 草稿
+define('POST_STATE_QUEUE',  	'queue'); 		// 博文发布状态: 队列
+
+
+
+
+
+
+class OAuth2_Provider_DianDian extends OAuth2_Provider 
+{
+
+	/**
+	 * @var  string  the method to use when requesting tokens
+	 */
+	public $method = 'POST';
+	/**
+	 * @var   host
+	 */
+	public $host = "https://api.diandian.com/v1/";
+	/**
+	 * @var   return data
+	 */
+	public $format = 'json';
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 * set property
+	 * -----------------------------
+	 *
+	 * @param property_name
+	 * @param value
+	 * @return void
+	 */
+	function __set($property_name, $value)
+	{
+		$this->$property_name = $value;	
+	}
+
+	/**
+	 * -----------------------------
+	 * Oauht2 授权地址
+ 	 * -----------------------------
+	 */
+	public function url_authorize()
+	{
+		return 'https://api.diandian.com/oauth/authorize';
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 * 换取access_token 请求地址
+ 	 * -----------------------------
+	 */
+	public function url_access_token()
+	{
+		return 'https://api.diandian.com/oauth/token';
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 * 获取access_token 方式为 Client Credentials
+ 	 * -----------------------------
+	 */
+	private function _app_access_token()
+	{
+		// $this->method = 'GET';
+		$param = array(
+			'grant_type' => 'client_credentials',
+			'scope'      => 'read,write'
+			);
+		$token  = $this->access('', $param);
+		return isset($token['access_token']) ? $token['access_token'] : '';
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取access_token 方式为 password
+ 	 * -----------------------------
+	 */
+	private function _password_access_token()
+	{
+		$param = array(
+			'grant_type' => 'password',
+			'scope'      => 'read,write'
+			);
+		$token  = $this->access('', $param);
+		return isset($token['access_token']) ? $token['access_token'] : '';
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  发起api请求前处理函数
+	 * - 处理 api 请求地址
+	 * - 处理 access_token
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param url  api地址或方法
+ 	 * @return array 公共api请求参数
+	 */
+	private function _req_before($url)
+	{	
+		if (strrpos($url, 'http://') !== 0 && strrpos($url, 'https://') !== 0) 
+			$this->api_url = "{$this->host}{$url}";
+
+		if($this->access_token){
+			$params['access_token'] = $this->access_token;
+		}else{
+			$params['access_token'] = $this->_password_access_token();
+		}
+		return $params;
+	}
+
+	// --------------------------------------------------------------------
+	// ================================
+	// = ========== 用户接口 ========== =
+	// ================================
+
+	/**
+	 * -----------------------------
+	 *  根据指定的api方法, 获取用户相关信息
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param method 	api方法
+ 	 * @param page 		页码
+ 	 * @return array    用户相关信息集合
+	 */
+	private function _get_user($method, $page = NULL)
+	{
+		$params = $this->_req_before($method);
+		if($page)
+		{
+			$params['limit']  = $this->page_count;
+			$params['offset'] = ($page - 1);
+		}
+		return $this->get($params);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取用户账号相关信息
+	 *  包括关注博客数, 用户喜欢post数等
+ 	 * -----------------------------
+ 	 * 
+ 	 * @link http://doc.diandian.com/api/userinfo/
+ 	 * @return array    用户账号相关信息集合
+	 */
+	public function get_user_info()
+	{
+		return $this->_get_user('user/info');
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取用户所喜欢的博客文章
+	 * - 包括文章详细信息, 喜欢的文章总数
+ 	 * -----------------------------
+ 	 * 
+ 	 * @link http://doc.diandian.com/api/likes/
+ 	 * @param page 		页码
+ 	 * @return array    用户喜欢的博文集合
+	 */
+	public function get_user_likes($page = 1)
+	{
+		return $this->_get_user('user/likes', $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取用户的所有关注
+	 * - 包括关注blog数, 关注的博客信息, 博客名
+	 *   博客域名, 最近更新时间
+ 	 * -----------------------------
+ 	 *
+ 	 * @link http://doc.diandian.com/api/following/
+ 	 * @param page 		页码
+ 	 * @return array    用户关注的博客集合
+	 */
+	public function get_user_follow($page =1)
+	{
+
+		return $this->_get_user('user/following', $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取用户订阅的标签
+	 * - 包括标签数组
+ 	 * -----------------------------
+ 	 *
+ 	 * @link http://doc.diandian.com/api/mytags/
+ 	 * @return array    用户订阅的标签集合
+	 */
+	public function get_user_tag()
+	{
+		return $this->_get_user('tag/mytags');
+	}
+
+
+	// --------------------------------------------------------------------
+	// ================================
+	// = ========== 博客接口 ========== =
+	// ================================
+
+
+	/**
+	 * -----------------------------
+	 *  获取博客信息
+ 	 * -----------------------------
+ 	 *
+ 	 * @link http://doc.diandian.com/api/blogInfo/
+ 	 * @param domain 	博客域名
+ 	 * @return array    博客信息集合
+	 */
+	public function get_blog_info($domain)
+	{
+		$url = 'blog/' . $domain . '/info';
+		$params = $this->_req_before($url);
+		return $this->get($params);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取头像url
+ 	 * -----------------------------
+ 	 *
+ 	 * @link http://doc.diandian.com/api/avatar/
+ 	 * @param domain 	博客域名
+ 	 * @param size 		只提供57x57,72x72,114x114,144x144大小的头像 默认:114
+ 	 * @return array    blog头像url地址
+	 */
+	public function get_blog_avatar($domain, $size = 114)
+	{
+		$url = 'blog/'.$domain.'/avatar/'.$size;
+		$this->_req_before($url);
+		return $this->api_url;
+	} 
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取博客粉丝
+ 	 * -----------------------------
+ 	 *
+ 	 * @link http://doc.diandian.com/api/followers/
+ 	 * @param domain 	博客域名
+ 	 * @param page 		页码
+ 	 * @return array    用户订阅的标签集合
+	 */
+	public function get_blog_fans($domain, $page = 1)
+	{
+		$url              = 'blog/'.$domain.'/followers';
+		$params           = $this->_req_before($url);
+		$params['limit']  = $this->page_count;
+		$params['offset'] = ($page - 1);
+		return $this->get($params);
+	}
+	// --------------------------------------------------------------------
+	// ================================
+	// = ========== 动态接口 ========== =
+	// ================================
+
+	// --------------------------------------------------------------------
+	public function get_home_post()
+	{
+
+	}
+
+	// --------------------------------------------------------------------
+
+	public function get_tag_post($tag, $page = 1, $since_id = 0)
+	{
+		$params= $this->_req_before('tag/posts/'.$tag);
+		$params['offset'] = ($page - 1);
+		$params['limit'] = $this->page_count;
+		$params['reblogInfo'] = true;
+		if($since_id != 0) $params['sinceId'] = $since_id;
+		return $this->get($params);
+	}
+
+	// --------------------------------------------------------------------
+	// ================================
+	// = ========== 文章接口 ========== =
+	// ================================
+
+	/**
+	 * -----------------------------
+	 *  获取博客文章
+	 * - 支持分页获取
+	 * - 支持指定id获取，此时type,tag,limit,offset将失效。
+	 * - 支持按文章类型获取, 文章类型支持:
+	 *   -- 文字 BLOG_POST_TYPE_TEXT
+	 *   -- 图片 BLOG_POST_TYPE_PHOTO
+	 *   -- 音乐 BLOG_POST_TYPE_AUDIO
+	 *   -- 视频 BLOG_POST_TYPE_VIDEO
+	 *   -- 链接 BLOG_POST_TYPE_LINK
+	 * - 只支持单个tag获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param id 		文章id
+ 	 * @param type 		文章类型。不传为全部类型
+ 	 * @param tag 		文章标签 只支持单个
+ 	 * @param page 		页码 1为起始页
+ 	 * @return array 	博客文章信息集合
+	 */
+	private function _get_post($domain, $id = 0, $type = BLOG_POST_TYPE_ALL, $tag = NULL, $page = 1)
+	{
+		$url = 'blog/' . $domain . '/posts';
+		if($type != BLOG_POST_TYPE_ALL) $url .= '/'.$type;
+		$params = $this->_req_before($url);
+
+		if($id != 0){
+			$params['id'] = $id ;
+		}else{
+			if($tag) $params['tag'] = $tag;
+			$params['offset'] = ($page - 1);
+			$params['limit']  = $this->page_count;
+		}
+		return $this->get($params);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  根据博客文章id获取该文章详细信息
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param id 		文章id
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_by_id($domain, $id)
+	{
+		return $this->_get_post($domain, $id);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取所有类型博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0 , BLOG_POST_TYPE_ALL, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取 "文字类型" 博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_with_text($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0, BLOG_POST_TYPE_TEXT, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取 "图片类型" 博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_with_photo($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0, BLOG_POST_TYPE_PHOTO, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取 "音乐类型" 博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_with_audio($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0, BLOG_POST_TYPE_AUDIO, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取 "视频类型" 博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_with_video($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0, BLOG_POST_TYPE_VIDEO, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 *  获取 "链接类型" 博客文章
+	 * - 支持过滤标签
+	 * - 支持分页获取
+ 	 * -----------------------------
+ 	 * 
+ 	 * @param domain 	博客域名
+ 	 * @param page  	页码 1为起始页
+ 	 * @param tag 		标签
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function get_post_with_link($domain, $page = 1, $tag = NULL)
+	{
+		return $this->_get_post($domain, 0, BLOG_POST_TYPE_LINK, $tag, $page);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * -----------------------------
+	 * 发布博客文章
+ 	 * -----------------------------
+ 	 * 
+ 	 * @return array 	指定文章信息集合	
+	 */
+	public function _build_post_param($domain, $type, $state, $tag, $slug, $markdown)
+	{
+		$url                = '/blog/' . $domain . '/post';
+		$params             = $this->_req_before($url);
+		$params['type']     = $type;
+		$params['state']   = $state;
+		$params['markdown'] = $markdown;
+		if($tag) $params['tag'] = is_array($tag) ? implode(',', $tag) : $tag;
+		if($slug) $params['slug'] = $slug;
+		return $params;
+	}
+
+	// --------------------------------------------------------------------
+
+	public function publish_text($domain, $title = '', $body = '', $tag = NULL, $slug = NULL, $state = POST_STATE_PUBLISHED,  $markdown = false)
+	{
+		$params = $this->_build_post_param($domain, BLOG_POST_TYPE_TEXT, $state, $tag, $slug, $markdown);
+		if(!empty($title) || !empty($body)){
+			$params['title'] = $title;
+			$params['body'] = $body;
+			return $this->post($params);
+		}else{
+			return false;
+		}
+	}
+
+	// --------------------------------------------------------------------
+
+	public function publish_photo($domain, $img, $caption = '', $tag = NULL, $slug = NULL, $state = POST_STATE_PUBLISHED,  $markdown = false)
+	{
+		$params = $this->_build_post_param($domain, BLOG_POST_TYPE_TEXT, $state, $tag, $slug, $markdown);
+		$params['caption'] = $caption;
+		$params['data'] = $img;
+	}
+
+	// --------------------------------------------------------------------
+
+
+}
